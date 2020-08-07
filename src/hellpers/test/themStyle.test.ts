9ab83c5765cc3347f38bbb999d89c945e4ce2722
Mladen <blagmash@gmail.com>
2020-08-07 13:58:27 +0200

    theme tested

--- src/hellpers/test/themStyle.test.ts
+++ src/hellpers/test/themStyle.test.ts
@@ -1,7 +1,15 @@
 import themeStyle from "../theme";
 
-global.localStorage.setItem("theme", "dark");
+test("maps every item in aray with local storage value(null)", () => {
+  expect(themeStyle(["m"])).toStrictEqual(["whitem"]);
+});
 
-test("maps evry item in aray with local storage value", () => {
+test("maps every item in aray with local storage value(dark)", () => {
+  global.localStorage.setItem("theme", "dark");
   expect(themeStyle(["m"])).toStrictEqual(["darkm"]);
+});
+
+test("maps every item in aray with local storage value(white)", () => {
+  global.localStorage.setItem("theme", "white");
+  expect(themeStyle(["m"])).toStrictEqual(["whitem"]);
 });
--- src/hellpers/theme.tsx
+++ src/hellpers/theme.tsx
@@ -6,9 +6,14 @@ type stringArray = string[];
 
 const themeStyle = (unmapedArray: stringArray): stringArray => {
   const activeTheme = localStorage.getItem("theme");
-  const mapedArray = unmapedArray.map((style) => activeTheme + style);
-
-  return mapedArray;
+  if (activeTheme === null) return mapppedArray(unmapedArray, "white");
+  return mapppedArray(unmapedArray, activeTheme);
 };
 
 export default themeStyle;
+
+const mapppedArray = (unmapedArray, theme) => {
+  const mapedArray = unmapedArray.map((style) => theme + style);
+
+  return mapedArray;
+};
