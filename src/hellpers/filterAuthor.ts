03bd5f038e9e4dc3861c59977c59a02615cb6ff9
Mladen <blagmash@gmail.com>
2020-08-13 09:33:38 +0200

    author hellper

---
+++ src/hellpers/filterAuthor.ts
@@ -0,0 +1,20 @@
+const filter = (string, filter, obj) => {
+  const array = arrayMy(string);
+  if (
+    array.some((x) => {
+      return equalCallback(x, filter);
+    })
+  ) {
+    return obj;
+  }
+};
+
+const arrayMy = (string) => {
+  return string.split(" ");
+};
+
+const equalCallback = (arg1, arg2) => {
+  return arg1.toLowerCase() === arg2.toLowerCase();
+};
+
+export default filterAuthor;
--- src/pages/profilePage.tsx
+++ src/pages/profilePage.tsx
@@ -4,12 +4,13 @@ import useInputHook from "../customHooks/inputHook";
 import { Heeaders } from "../actions/Headers";
 import getRequest from "../actions/getReequest";
 import { api } from "../enviroment/api";
+import filterAuthor from "../hellpers/filterAuthor";
 
 const ProfilePage = () => {
   const [page, setPage] = useState<any>(0);
   const [result, setResult] = useState<any>(undefined);
   const [filter, search] = useInputHook("", "search", "Search quote");
-  const [tags, setTags] = useState<any>();
+  const [tags, setTags] = useState<any[]>();
   const [authors, setAuthors] = useState<any>();
 
   const Config = {
@@ -19,7 +20,10 @@ const ProfilePage = () => {
 
   const quotesCallback = (arg) => {
     if (arg) {
+      console.log(arg);
       setResult(arg);
+
+      console.log(tags);
     }
 
     return;
@@ -27,6 +31,15 @@ const ProfilePage = () => {
 
   const typeheadCallback = (arg) => {
     console.log(arg);
+    const { tags } = arg;
+    const { authors } = arg;
+
+    const fil = new RegExp(filter, "i");
+
+    setAuthors(
+      authors.filter((author) => filterAuthor(author.name, filter, author))
+    );
+    setTags(tags.filter((tag) => tag.name.match(filter)));
   };
   const getQuotes = () => {
     getRequest(api.quotes(), quotesCallback, Config);
@@ -52,9 +65,36 @@ const ProfilePage = () => {
           filter={filter}
         />
       </div>
-      <div className="sidebar">sidebar</div>.
+      <div className="sidebar">
+        {authors ? (
+          <div className="tags flexColumn ">
+            <h1>Author</h1>
+            {authors.map((author) => (
+              <Tag key={author.name} tag={author} />
+            ))}
+          </div>
+        ) : null}
+
+        {tags ? (
+          <div className="tags flexColumn ">
+            <h1>Tags</h1>
+            {tags.map((tag) => (
+              <Tag key={tag.name} tag={tag} />
+            ))}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
 
 export default ProfilePage;
+
+const Tag = ({ tag }) => {
+  return (
+    <div className="flexRow spaceAround">
+      <p>{tag.name}</p>
+      <p>{tag.count}</p>
+    </div>
+  );
+};
