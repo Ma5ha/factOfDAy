f5dac411034d428ad0555dfe10388af09f7187d8
Mladen <blagmash@gmail.com>
2020-08-22 10:39:22 +0200

    highligher refactored form better maching

--- src/hellpers/highlight.tsx
+++ src/hellpers/highlight.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 const highlight = (str: string, filter: string) => {
+  const regExp = new RegExp(filter, "i");
   if (filter.length === 0) {
     return str;
   }
 
-  if (str.length !== 0 && str.match(filter) !== null) {
-    const startIndex = str.match(filter)?.index;
+  if (str.length !== 0 && str.match(regExp) !== null) {
+    const startIndex = str.match(regExp).index;
+    const match = str.slice(startIndex, startIndex + filter.length);
     const prefix = str.slice(0, startIndex);
     const sulfix = str.slice(startIndex + filter.length);
     return (
       <>
         {prefix}
-        <mark>{filter}</mark>
+        <mark>{match}</mark>
         {highlight(sulfix, filter)}
       </>
     );
