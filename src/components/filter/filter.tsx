3c48fed7f350c78662f429bd4cbbae010e805700
Mladen <blagmash@gmail.com>
2020-08-25 14:51:20 +0200

    filter

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-const Filter = ({ tag }) => {
+const Filter = ({ data: { name, count }, index }) => {
+  const animation = `fade-in-bottom 2s cubic-bezier(0.39, 0.575, 0.565, 1) ${index}s both`;
+
   return (
-    <div className="flexRow spaceAround filter fade-in-bottom">
+    <div className="flexRow spaceAround filter" style={{ animation }}>
       <div className="name">
-        <p>{tag.name}</p>
+        <p>{name}</p>
       </div>
       <div className="number">
-        <p>{tag.count}</p>
+        <p>{count}</p>
       </div>
     </div>
   );
