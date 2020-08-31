36a68eacf2e1bedb4f6565f0d313dda47f7b863b
Mladen <blagmash@gmail.com>
2020-08-31 09:17:46 +0200

    slide but bad

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,31 +1,91 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./carousel.css";
+
 const Carousel = ({ children }) => {
-  const [index, setIndex] = useState(0);
+  let enter = { className: "slideInRight" };
+  let exit = { className: "slideOutLeft" };
+  let enterA = { className: "slideInLeft" };
+  let exitA = { className: "slideOutRight" };
+  const [animation, setAnimation] = useState(true);
+  const [slide, setSlide] = useState(0);
 
   const next = () => {
-    if (index <= children.length - 2) {
-      setIndex(index + 1);
+    if (!animation) {
+      setAnimation(true);
+    }
+    if (slide <= children.length - 2) {
+      setSlide(slide + 1);
+
       return;
     }
-    setIndex(0);
+    setSlide(0);
   };
 
   const previous = () => {
-    if (index === 0) {
-      setIndex(children.length - 1);
+    if (animation) {
+      setAnimation(false);
+    }
+
+    if (slide === 0) {
+      setSlide(children.length - 1);
+
       return;
     }
-    setIndex(index - 1);
+    setSlide(slide - 1);
   };
 
   return (
-    <div className="carousel">
-      {children[index]}
-      <button onClick={next}>next</button>
-      <button onClick={previous}>prev</button>
+    <div className="flexColumn flexCenter autoMargin">
+      <div className="carousel autoMargin">
+        <div className="slideFrame  ">
+          <div className={"slideQuote autoMargin "}>
+            {animation ? (
+              <Slide animation={{ enter, exit }}>{children[slide]}</Slide>
+            ) : (
+              <Slide animation={{ enter: enterA, exit: exitA }}>
+                {children[slide]}
+              </Slide>
+            )}
+          </div>
+          <div className={"slideQuote autoMargin "}></div>
+
+          <div className="slides"></div>
+        </div>
+      </div>
+      <div className="buttons autoMargin">
+        <button onClick={previous}>prev</button>
+        <button onClick={next}>next</button>
+      </div>
     </div>
   );
 };
 
 export default Carousel;
+
+const Slide = ({ children, animation }) => {
+  const { enter, exit } = animation;
+
+  const [onDisplay, setOnDisplay] = useState<any>();
+  const [componentEntered, setComponent] = useState<boolean>();
+
+  useEffect(() => {
+    setComponent(true);
+  }, [children]);
+
+  const handlleExit = () => {
+    setComponent(false);
+    setOnDisplay(children);
+  };
+  return componentEntered ? (
+    <div
+      {...exit}
+      onAnimationEnd={() => {
+        handlleExit();
+      }}
+    >
+      {onDisplay}
+    </div>
+  ) : (
+    <div {...enter}>{onDisplay}</div>
+  );
+};
--- src/pages/author.tsx
+++ src/pages/author.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Children, useLayoutEffect } from "react";
 import { useParams } from "react-router-dom";
 import getRequest from "../actions/getReequest";
 import { api } from "../enviroment/api";
