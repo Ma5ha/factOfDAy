638ec3f56202e0bc76026a6821edb2ba14fe1cf0
Mladen <blagmash@gmail.com>
2020-09-11 22:06:12 +0200

    working modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -6,9 +6,26 @@ import "./modal.css";
 const Modal = ({ handlleClick }) => {
   const modal = `${themeStyle(["Modal"]).join("")} modal`;
 
+  const close = (e) => {
+    if (e.target.className === "modalWarper") {
+      handlleClick();
+    }
+  };
   return (
-    <div onClick={handlleClick} className="modalWarper">
-      <div className={modal}>mashasha</div>
+    <div onClick={close} className="modalWarper" tabIndex={-1}>
+      <div
+        //    onBlur={handlleClick}
+
+        tabIndex={1}
+        className={modal}
+      >
+        <form>
+          <label>Author :</label>
+          <input type="text" placeholder="Author name" />
+          <label>Quote</label>
+          <textarea></textarea>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/modal/modalButton.tsx
+++ src/components/modal/modalButton.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import themeStyle from "../../hellpers/theme";
 import "./modal.css";
 const ModalButton = ({ clickHandler }) => {
