a9723dcdec44b1ba040decb5e1dc201779b58efd
Mladen <blagmash@gmail.com>
2020-09-14 12:49:51 +0200

    searchBar cleared

--- src/components/search/searchBar.tsx
+++ src/components/search/searchBar.tsx
@@ -6,22 +6,13 @@ import {
   spaceAround,
   flexRow,
   flexCenter,
-  justifayCenter,
 } from "../../styles/style.var";
 import inputFactory from "../../hellpers/inputFactory";
 import styleToggle from "../../hellpers/styleToggle";
-import Author from "../quote/author";
-import Quote from "../quote/quote";
-import Votes from "../quote/votes";
-import { Quote as Quot } from "../quote/quoteTypes";
+
 import "./shearchBar.css";
 
 import themeStyle from "../../hellpers/theme";
-import hart from "../../assets/heart.svg";
-import like from "../../assets/like.svg";
-
-import dislike from "../../assets/dislike.svg";
-import { api } from "../../enviroment/api";
 
 const SearchBar = ({ handleSubmit, search, result }) => {
   return (
